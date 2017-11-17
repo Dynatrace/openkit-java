5b21e55f0f33df300816ecf297a5d423731ac0dd
Stefan Eberl <stefan.eberl@dynatrace.com>
2017-11-17 11:34:11 +0100

    Default server ID for Dynatrace is now 1

--- src/main/java/com/dynatrace/openkit/core/configuration/OpenKitType.java
+++ src/main/java/com/dynatrace/openkit/core/configuration/OpenKitType.java
@@ -5,22 +5,22 @@ package com.dynatrace.openkit.core.configuration;
  */
 public enum OpenKitType {
 
-	APPMON("dynaTraceMonitor", 1),			// AppMon: default monitor URL name contains "dynaTraceMonitor" and default Server ID is 1
-	DYNATRACE("mbeacon", -1);				// Dynatrace: default monitor URL name contains "mbeacon" and default Server ID is -1
+    APPMON("dynaTraceMonitor", 1),            // AppMon: default monitor URL name contains "dynaTraceMonitor" and default Server ID is 1
+    DYNATRACE("mbeacon", 1);                // Dynatrace: default monitor URL name contains "mbeacon" and default Server ID is 1
 
-	private String defaultMonitorName;
-	private int defaultServerID;
+    private String defaultMonitorName;
+    private int defaultServerID;
 
-	OpenKitType(String defaultMonitorName, int defaultServerID) {
-		this.defaultMonitorName = defaultMonitorName;
-		this.defaultServerID = defaultServerID;
-	}
+    OpenKitType(String defaultMonitorName, int defaultServerID) {
+        this.defaultMonitorName = defaultMonitorName;
+        this.defaultServerID = defaultServerID;
+    }
 
-	public String getDefaultMonitorName() {
-		return defaultMonitorName;
-	}
+    public String getDefaultMonitorName() {
+        return defaultMonitorName;
+    }
 
-	public int getDefaultServerID() {
-		return defaultServerID;
-	}
+    public int getDefaultServerID() {
+        return defaultServerID;
+    }
 }
