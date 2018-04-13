d39889c077fb06a8d9569889e3692e14cab5c565
Johannes Baeuerle <johannes.baeuerle@dynatrace.com>
2018-04-13 11:53:14 +0200

    DefaultThreadIDProvider util for int thread ids
    tests are now possible because of the util function

--- src/main/java/com/dynatrace/openkit/providers/DefaultThreadIDProvider.java
+++ src/main/java/com/dynatrace/openkit/providers/DefaultThreadIDProvider.java
@@ -24,9 +24,25 @@ public class DefaultThreadIDProvider implements ThreadIDProvider {
     @Override
     public int getThreadID() {
         long threadID64 = Thread.currentThread().getId();
-        int hash = (int)((threadID64 ^ (threadID64 >>> 32)) & 0x7fffffff );
-
-        return hash;
+        return convertNativeThreadIDToPositiveInteger(threadID64);
     }
 
+    /*
+     * Thread.currentThread().getId() returns a long value.
+     * The Beacon protocol requires the thread id to be a positive integer value. By using the xor operation
+     * between higher and lower 32 bits of the long value we get an integer value. The returned integer
+     * can be negative though.
+     * Therefore  the most significant bit is forced to '0' by a bitwise-and operation with an integer
+     * where all bits except for the most significant bit are set to '1'.
+     */
+
+    /**
+     * Converts a native thread id to a positive integer Thread.currentThread().getId()
+     * @param nativeThreadID the native thread id returned by
+     * @return
+     */
+    public static int convertNativeThreadIDToPositiveInteger(long nativeThreadID)
+    {
+        return (int)((nativeThreadID ^ (nativeThreadID >>> 32)) & 0x7fffffff );
+    }
 }
--- src/test/java/com/dynatrace/openkit/providers/DefaultThreadIDProviderTest.java
+++ src/test/java/com/dynatrace/openkit/providers/DefaultThreadIDProviderTest.java
@@ -33,4 +33,38 @@ public class DefaultThreadIDProviderTest {
         // then
         assertThat(provider.getThreadID(), is(equalTo(threadHash)));
     }
+
+    @Test
+    public void convertNativeThreadIDToPositiveIntegerVerifyXorBitPatterns() {
+        // given
+        long testValue = 0x0000000600000005L; // bytes 0101 and 0110 -> xor resulting in 0011
+
+        // when
+        int result = DefaultThreadIDProvider.convertNativeThreadIDToPositiveInteger(testValue);
+        // then
+        assertThat(result, is(equalTo(3)));
+    }
+
+    @Test
+    public void convertNativeThreadIDToPositiveIntegerVerifyMaskMSBFirst() {
+        // given
+        long testValue = 1L << 31;
+
+        // when
+        int result = DefaultThreadIDProvider.convertNativeThreadIDToPositiveInteger(testValue);
+        // then
+        assertThat(result, is(equalTo(0)));
+    }
+
+    @Test
+    public void convertNativeThreadIDToPositiveIntegerVerifyMaskMSBSecond() {
+        // given
+        long testValue = 1L << 63;
+
+        // when
+        int result = DefaultThreadIDProvider.convertNativeThreadIDToPositiveInteger(testValue);
+        // then
+        assertThat(result, is(equalTo(0)));
+    }
+
 }
