141c2ea4d2c8a006d151cddd8a0164be3919d3ff
Stefan Eberl <stefan.eberl@dynatrace.com>
2018-09-07 15:32:34 +0200

    Remove empty line

--- src/main/java/com/dynatrace/openkit/protocol/Beacon.java
+++ src/main/java/com/dynatrace/openkit/protocol/Beacon.java
@@ -836,7 +836,6 @@ public class Beacon {
      * in case of level 2 (USER_BEHAVIOR) the value from the configuration is used
      * in case of level 1 (PERFORMANCE) or 0 (OFF) a random number in the positive Long range is used
      *
-
      * @return The device identifier, which is truncated to 250 characters if level 2 (USER_BEHAVIOR) is used.
      */
     public String getDeviceID() {
