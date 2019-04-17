514b3d20745246d6fd92d84edf63f660d49b36a2
Stefan Eberl <stefan.eberl@dynatrace.com>
2019-04-17 09:54:41 +0200

    Merge pull request #5 in OP/openkit-java from feature/APM-166913-JSON-boolean-value to master
    * commit 'f8c20baa6d385491b25e017ea98d179721ec7be9':
      Implement JSON boolean value
    
    GitOrigin-RevId: 96416d04a6f6497e123d8ad9a6ddf89a2b58c820

---
+++ src/main/java/com/dynatrace/openkit/util/json/objects/JSONBooleanValue.java
@@ -0,0 +1,95 @@
+/**
+ * Copyright 2018-2019 Dynatrace LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.dynatrace.openkit.util.json.objects;
+
+import com.dynatrace.openkit.util.json.constants.JSONLiterals;
+
+/**
+ * JSON value class representing a boolean value.
+ */
+public class JSONBooleanValue extends JSONValue {
+
+    /** singleton instance representing true. */
+    public static final JSONBooleanValue TRUE = new JSONBooleanValue(true);
+
+    /** singleton instance representing false. */
+    public static final JSONBooleanValue FALSE = new JSONBooleanValue(false);
+
+    /** The boolean value represented by this instance. */
+    private final boolean value;
+
+    /**
+     * Constructor taking the boolean value which is represented by this instance.
+     *
+     * <p>
+     *     Use the factory methods {@link #fromValue(boolean)} or {@link #fromLiteral(String)} instead.
+     * </p>
+     *
+     * @param value The boolean value represented by this instance.
+     */
+    private JSONBooleanValue(boolean value) {
+        this.value = value;
+    }
+
+    /**
+     * Factory method to create a {@link JSONBooleanValue} from a given boolean value.
+     *
+     * <p>
+     *     The return value is one of the two singleton instances {@link #TRUE} or {@link #FALSE}, depending on {@code value}.
+     * </p>
+     *
+     * @param value The boolean value.
+     * @return Returns either {@link #FALSE} or {@link #TRUE}.
+     */
+    public static JSONBooleanValue fromValue(boolean value) {
+        return value ? TRUE : FALSE;
+    }
+
+    /**
+     * Factory method to create a {@link JSONBooleanValue} from a given JSON literal.
+     *
+     * @param literal The string literal for which to create the value.
+     *
+     * @return {@link #TRUE} if literal is {@link JSONLiterals#BOOLEAN_TRUE_LITERAL}, {@link #FALSE} if literal
+     *      is {@link JSONLiterals#BOOLEAN_FALSE_LITERAL} and {@code null} in every other case.
+     */
+    public static JSONBooleanValue fromLiteral(String literal) {
+        if (literal == null) {
+            return null;
+        } else if (literal.equals(JSONLiterals.BOOLEAN_TRUE_LITERAL)) {
+            return TRUE;
+        } else if (literal.equals(JSONLiterals.BOOLEAN_FALSE_LITERAL)) {
+            return FALSE;
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public boolean isBoolean() {
+        return true;
+    }
+
+    /**
+     * Get value represented by this instance.
+     *
+     * @return Boolean value represented by this instance.
+     */
+    public boolean getValue() {
+        return value;
+    }
+}
---
+++ src/test/java/com/dynatrace/openkit/util/json/objects/JSONBooleanValueTest.java
@@ -0,0 +1,123 @@
+/**
+ * Copyright 2018-2019 Dynatrace LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.dynatrace.openkit.util.json.objects;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertThat;
+
+public class JSONBooleanValueTest {
+
+    @Test
+    public void isNullReturnsFalse() {
+        // then
+        assertThat(JSONBooleanValue.fromValue(true).isNull(), is(false));
+    }
+
+    @Test
+    public void isBooleanReturnsTrue() {
+        // then
+        assertThat(JSONBooleanValue.fromValue(true).isBoolean(), is(true));
+    }
+
+    @Test
+    public void isNumberReturnsFalse() {
+        // then
+        assertThat(JSONBooleanValue.fromValue(true).isNumber(), is(false));
+    }
+
+    @Test
+    public void isStringReturnsFalse() {
+        // then
+        assertThat(JSONBooleanValue.fromValue(true).isString(), is(false));
+    }
+
+    @Test
+    public void isArrayReturnsFalse() {
+        // then
+        assertThat(JSONBooleanValue.fromValue(true).isArray(), is(false));
+    }
+
+    @Test
+    public void isObjectReturnsFalse() {
+        // then
+        assertThat(JSONBooleanValue.fromValue(true).isObject(), is(false));
+    }
+
+    @Test
+    public void getValueReturnsAppropriateBooleanValue() {
+        // then
+        assertThat(JSONBooleanValue.TRUE.getValue(), is(true));
+        assertThat(JSONBooleanValue.FALSE.getValue(), is(false));
+    }
+
+    @Test
+    public void fromValueReturnsTrueSingletonValue() {
+        // when
+        JSONBooleanValue obtained = JSONBooleanValue.fromValue(true);
+
+        // then the singleton TRUE instance is returned
+        assertThat(obtained, is(sameInstance(JSONBooleanValue.TRUE)));
+    }
+
+    @Test
+    public void fromValueReturnsFalseSingletonValue() {
+        // when
+        JSONBooleanValue obtained = JSONBooleanValue.fromValue(false);
+
+        // then the singleton FALSE instance is returned
+        assertThat(obtained, is(sameInstance(JSONBooleanValue.FALSE)));
+    }
+
+    @Test
+    public void fromLiteralGivesFalseIfLiteralIsNull() {
+        // then
+        assertThat(JSONBooleanValue.fromLiteral(null), is(nullValue()));
+    }
+
+    @Test
+    public void fromLiteralReturnsTrueSingletonValueForTrueLiteral() {
+        // when
+        JSONBooleanValue obtained = JSONBooleanValue.fromLiteral("true");
+
+        // then the singleton FALSE instance is returned
+        assertThat(obtained, is(sameInstance(JSONBooleanValue.TRUE)));
+    }
+
+    @Test
+    public void fromLiteralReturnsFalseSingletonValueForFalseLiteral() {
+        // when
+        JSONBooleanValue obtained = JSONBooleanValue.fromLiteral("false");
+
+        // then the singleton FALSE instance is returned
+        assertThat(obtained, is(sameInstance(JSONBooleanValue.FALSE)));
+    }
+
+    @Test
+    public void fromLiteralReturnsNullForNonBooleanLiterals() {
+        // when wrong casing is used, then
+        assertThat(JSONBooleanValue.fromLiteral("TRUE"), is(nullValue()));
+        assertThat(JSONBooleanValue.fromLiteral("FALSE"), is(nullValue()));
+
+        // and when it's actually a number, then
+        assertThat(JSONBooleanValue.fromLiteral("1234"), is(nullValue()));
+
+        // and when passed an empty string, then
+        assertThat(JSONBooleanValue.fromLiteral(""), is(nullValue()));
+    }
+}
